7a14e441526771a7469242080995312aa5761f68
aaron-wheeler <77795743+aaron-wheeler@users.noreply.github.com>
2023-05-16 11:46:03 -0400

    update test params

--- test/example_FT.jl
+++ test/example_FT.jl
@@ -5,10 +5,10 @@ parameters = (
     # init_cash_range = 10000.0:0.01:30000.0,
     # init_shares_range = 0:1:120,
     init_cash_range = 5000.0:0.01:15000.0,
-    init_shares_range = 0:1:60,
+    init_shares_range = 50:1:150,
     prob_wait = 0.5, # probability of halting (per active trader)
-    trade_freq = 9, # how many seconds to wait (if `prob_wait` invoked)
-    num_ids = 30 + 30 # number of reserved ids set aside for other agents
+    trade_freq = 10, # how many seconds to wait (if `prob_wait` invoked)
+    num_ids = 30 + 70 # number of reserved ids set aside for other agents
 )
 
 server_info = (
@@ -18,9 +18,9 @@ server_info = (
     password = "value123"
 )
 
-num_traders, num_assets = 30, 1
+num_traders, num_assets = 70, 1
 market_open = Dates.now() + Dates.Second(8)
-market_close = market_open + Dates.Minute(30)
+market_close = market_open + Dates.Minute(60)
 
 FT_run(num_traders, num_assets, market_open, market_close, parameters, server_info, print_msg=true)
 
